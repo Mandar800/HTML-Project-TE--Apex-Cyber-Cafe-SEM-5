19078f848db6d34add9ac1f1b08ed4f4a676cf05
Mandar Salvi <salvim800@gmail.com>
2019-10-04 14:16:16 +0530

    2.4 google maps

---
+++ gmap.js
@@ -0,0 +1,7 @@
+function myMap() {
+var mapProp= {
+center:new google.maps.LatLng(19.020518, 72.868985),
+zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
